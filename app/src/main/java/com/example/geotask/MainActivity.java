82affd3c1aa27bbb8d420b79d9d462d9014acb1d
Виталий <vite184@yandex.ru>
2021-08-27 21:54:35 +0300

    Added Yandex MapKit

--- app/src/main/java/com/example/geotask/MainActivity.java
+++ app/src/main/java/com/example/geotask/MainActivity.java
@@ -3,11 +3,43 @@ package com.example.geotask;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
 public class MainActivity extends Activity {
+    private final String MAPKIT_API_KEY = "";
+    private MapView mapView;
+    private final Point TARGET_LOCATION = new Point(55.755811, 37.617617);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapView);
+
+        mapView.getMap().move(
+                new CameraPosition(TARGET_LOCATION, 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
     }
 }
